10a64eabb3fe00ed9a8ebffd44ef34ffbf14ae82
edgarburgues <edgarfdieguez@gmail.com>
2024-07-06 17:51:48 +0200

    Refactor SVG template generation code

--- src/utils/svg_templates.js
+++ src/utils/svg_templates.js
@@ -28,7 +28,6 @@ function generateSvgContent(title, author, imgBase64Url, dominantColor1Hex, domi
   }
 
   return `
-    <!-- Hecho por @edgarburgues -->
     <svg width="500" height="200" xmlns="http://www.w3.org/2000/svg">
         <defs>
             <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
@@ -69,8 +68,7 @@ function generateSvgContentVertical(title, author, imgBase64Url, dominantColor1H
   }
 
   return `
-    <!-- Hecho por @edgarburgues -->
-    <svg width="200" height="250" xmlns="http://www.w3.org/2000/svg">
+\    <svg width="200" height="250" xmlns="http://www.w3.org/2000/svg">
         <defs>
             <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
                 <stop offset="0%" style="stop-color:${dominantColor1Hex};stop-opacity:1" />
