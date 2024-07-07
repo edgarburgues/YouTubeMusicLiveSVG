24bfecd30caa15695387d9621c21b1c6c321f358
edgarburgues <edgarfdieguez@gmail.com>
2024-07-07 14:59:59 +0200

    chore: Update SVG gradient definition in svg_templates.ts

--- src/utils/svg_templates.ts
+++ src/utils/svg_templates.ts
@@ -39,9 +39,9 @@ function generateSvgContent(
   return `
     <svg width="500" height="200" xmlns="http://www.w3.org/2000/svg">
         <defs>
-            <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
-                <stop offset="0%" style="stop-color:${dominantColor1Hex};stop-opacity:1" />
-                <stop offset="100%" style="stop-color:${dominantColor2Hex};stop-opacity:1" />
+            <linearGradient id="grad1" x1="0%" y1="0%" x2="100%">
+              <stop offset="0%" style="stop-color:${dominantColor1Hex};stop-opacity:1" />
+              <stop offset="100%" style="stop-color:${dominantColor2Hex};stop-opacity:1" />
             </linearGradient>
             <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
                 <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
