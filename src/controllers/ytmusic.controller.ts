2aa13061f1b3dec16668a24113d59426b4ad061a
edgarburgues <edgarfdieguez@gmail.com>
2024-07-07 15:24:40 +0200

    chore: Update YTMusicController to set no-cache headers

--- src/controllers/ytmusic.controller.ts
+++ src/controllers/ytmusic.controller.ts
@@ -11,7 +11,9 @@ class YTMusicController {
   static async getHistory(req: Request, res: Response) {
     try {
       const history = await ytmusic.getHistory();
-      setNoCacheHeaders(res);
+      res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+      res.setHeader("Pragma", "no-cache");
+      res.setHeader("Expires", "0");
       res.json(history);
     } catch (error: any) {
       logger.error(`Error fetching history: ${error.message}`);
@@ -24,7 +26,9 @@ class YTMusicController {
       const history = await ytmusic.getHistory();
       if (history.length > 0) {
         const firstVideo = history[0];
-        setNoCacheHeaders(res);
+        res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Expires", "0");
         res.json(firstVideo);
       } else {
         res.status(404).json({ error: "No video found in history" });
@@ -43,7 +47,9 @@ class YTMusicController {
         const { imgBase64Url, dominantColor1Hex, dominantColor2Hex } = await getImageAndPalette(firstVideo.thumbnail);
         const svgContent = generateSvgContent(firstVideo.song, firstVideo.author, imgBase64Url, dominantColor1Hex, dominantColor2Hex);
         res.setHeader("Content-Type", "image/svg+xml");
-        setNoCacheHeaders(res);
+        res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Expires", "0");
         res.send(svgContent);
       } else {
         res.status(404).json({ error: "No video found in history" });
